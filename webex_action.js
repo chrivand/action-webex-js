567d9794e045eeafa3b4414c212cae851b86de33
chrivand <chrivand@cisco.com>
2022-04-21 16:17:37 +0200

    Create webex_action.js

---
+++ webex_action.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+    var ROOMID = core.getInput('ROOMID');
+    var TOKEN = core.getInput('TOKEN');
+    var MESSAGE = core.getInput('MESSAGE');
+
+    var BODY = 'roomId=' + ROOMID + 'markdown=' + MESSAGE;
+
+    fetch('https://webexapis.com/v1/messages/', {
+        method: 'POST',
+        headers: {
+            'Authorization': TOKEN,
+        },
+        body: BODY
+    });
+} catch (error) {
+    core.setFailed(error.message);
+}
