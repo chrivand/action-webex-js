c26909b739fae1fb27f28ba7cd08e03954ff5da9
chrivand <chrivand@cisco.com>
2022-04-21 21:06:44 +0200

    Update webex_action.js

--- webex_action.js
+++ webex_action.js
@@ -3,11 +3,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 var ROOMID = process.env.ROOMID;
-console.log(ROOMID);
 var TOKEN = process.env.TOKEN;
 var MESSAGE = process.env.MESSAGE;
 
 var BODY = 'roomId=' + ROOMID + 'markdown=' + MESSAGE;
+console.log(BODY)
 
 axios
   .post('https://webexapis.com/v1/messages/', {
