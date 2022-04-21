8c384ddfcbee3d8e06738f16a309c3b8f5725da0
chrivand <chrivand@cisco.com>
2022-04-21 21:53:05 +0200

    new version of axios

--- webex_action.js
+++ webex_action.js
@@ -3,22 +3,28 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 var ROOMID = process.env.ROOMID;
-var TOKEN = 'Bearer ' + process.env.TOKEN;
-var MESSAGE = process.env.MESSAGE;
+var BEARER_TOKEN = 'Bearer ' + process.env.TOKEN;
+var MARKDOWN = process.env.MESSAGE;
 
-axios
-  .post('https://webexapis.com/v1/messages', {
-    roomId: ROOMID,
-    markdown: MESSAGE
-  }, {
-      headers: { 
-          Authorization: TOKEN
-      }
-  })
-  .then(res => {
-    console.log(`statusCode: ${res.status}`)
-    console.log(res)
-  })
-  .catch(error => {
-    console.error(error)
-  })
+var data = JSON.stringify({
+  "roomId": ROOMID,
+  "markdown": MARKDOWN,
+});
+
+var config = {
+  method: 'post',
+  url: 'https://webexapis.com/v1/messages',
+  headers: { 
+    'Authorization': BEARER_TOKEN, 
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
